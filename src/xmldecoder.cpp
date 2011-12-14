a2c93d63425edeb21279204cea8d99da8cb66b9c
Hendrik v. Raven <h.v.raven@googlemail.com>
2011-12-14 23:05:38 +0100

    cleaned old code and added new todos

--- src/xmldecoder.cpp
+++ src/xmldecoder.cpp
@@ -11,6 +11,7 @@
  * \todo change the elements map to a static version which isn't created newly
  * at every call
  * \todo add missing components
+ * \todo add error handling
  *
  * this function checks element by element what it is and calls the respecting
  * add functions to add them to the given spell.
@@ -232,6 +233,8 @@ void Spell_List::decode_casting_time_( Spell *const pspell,
  * adds the given component to the given spell. This function only decodes
  * the one given component and doesn't search for other components. In the
  * same way it only manipulates the one component setting.
+ *
+ * \todo add support for M/DF (with description)
  */
 void Spell_List::decode_component_( Spell *const pspell,
                                     TiXmlElement const *const pelement )
@@ -345,6 +348,8 @@ void Spell_List::decode_duration_( Spell *const pspell,
  * \brief add the saving throw information to the given spell
  * \param pspell spell to add the information to
  * \param pelement pointer to the saving throw in the xml
+ *
+ * \todo allow type combinations (half or partial)
  */
 void Spell_List::decode_saving_throw_( Spell *const pspell,
                                        TiXmlElement const *const pelement )
@@ -389,240 +394,3 @@ void Spell_List::decode_spell_resistance_( Spell *const pspell,
         == TIXML_SUCCESS) && temp ) work.set_resistance(true);
   pspell->set_spell_resistance(work);
 }
-
-/*
-Spell_Base_Element Spells::get_spell_spell_element_( TiXmlElement* pspell,
-						const std::string& search_for )
-{
-  TiXmlElement* pchild = pspell->FirstChildElement( search_for );
-  if ( pchild )
-    {
-      std::string type = pchild->Attribute( "type" );
-      if ( type != "" )
-	{
-	  int value = 0;
-	  pchild->QueryIntAttribute( "value", &value );
-	  if ( value )
-	    {
-	      Spell_Base_Element temp(type, value);
-	      return temp;
-	    }
-	  else
-	    {
-	      Spell_Base_Element temp(type);
-	      return temp;
-	    }
-	}
-      else
-	throw Missing_Element ( CASTING_TIME_TYPE );
-    }
-  else
-    throw Missing_Element ( CASTING_TIME );
-}
-
-Components Spells::get_spell_components_( TiXmlElement* pspell )
-{
-  TiXmlElement* pcomponent = pspell->FirstChildElement( "component" );
-  if ( pcomponent )
-    {
-      Components temp;
-      while ( pcomponent )
-	{
-	  std::string work = pcomponent->Attribute( "type" );
-	  switch ( work[0] )
-	    {
-	    case 'V':
-	      {
-		temp.set_verbal( true );
-		break;
-	      }
-	    case 'S':
-	      {
-		temp.set_somatic( true );
-		break;
-	      }
-	    case 'M':
-	      {
-		std::string description = pcomponent->GetText();
-		temp.set_material( true, description );
-		break;
-	      }
-	    case 'F':
-	      {
-		std::string description = pcomponent->GetText();
-		temp.set_focus( true, description);
-		break;
-	      }
-	    case 'D':
-	      {
-		if ( work[1] == 'F' )
-		  {
-		    temp.set_divine_focus( true );
-		    break;
-		  }
-	      }
-	    default:
-	      throw Invalid_Argument();
-	    }
-	  pcomponent = pcomponent->NextSiblingElement( "component" );
-	}
-      return temp;
-    }
-  else
-    throw Missing_Element ( COMPONENTS );
-}
-
-Saving_Throw Spells::get_spell_saving_throw_( TiXmlElement* pspell )
-{
-  TiXmlElement* psave = pspell->FirstChildElement( "saving_throw" );
-
-  if ( psave )
-    {
-      Saving_Throw temp;
-      TiXmlAttribute* pattr = psave->FirstAttribute();
-
-      while ( pattr )
-	{
-	  std::string work = pattr->Name();
-	  if ( work == "type" )
-	    temp.set_type( pattr->ValueStr() );
-	  else if ( work == "value" )
-	    temp.set_value( pattr->ValueStr() );
-	  else if ( work == "see_text" )
-	    {
-	      if ( pattr->ValueStr() == "yes" )
-		temp.set_see_text( true );
-	    }
-	  else if ( work == "harmless" )
-	    {
-	      if ( pattr->ValueStr() == "yes" )
-		temp.set_harmless( true );
-	    }
-	  pattr = pattr->Next();
-	}
-
-      return temp;
-    }
-  else
-    throw Missing_Element( SAVING_THROW );
-}
-
-Spell_Resistance Spells::get_spell_spell_resistance_( TiXmlElement* pspell )
-{
-  TiXmlElement* presist = pspell->FirstChildElement( "spell_resistance" );
-  Spell_Resistance temp;
-  if ( presist )
-    {
-      TiXmlAttribute* pattr = presist->FirstAttribute();
-      Spell_Resistance result;
-
-      while ( pattr )
-	{
-	  std::string work = pattr->Name();
-
-	  if ( work == "value" )
-	    {
-	      if ( pattr->ValueStr() == "yes" )
-		result.set_resistance( true );
-	      else
-		if ( pattr->ValueStr() == "no" )
-		  result.set_resistance( false );
-		else
-		  throw Invalid_Argument();
-	    }
-	  else if ( work == "see_text")
-	    {
-	      if ( pattr->ValueStr() == "yes" )
-		result.set_see_text( true );
-	      else
-		if ( pattr->ValueStr() == "no" )
-		  result.set_see_text( false );
-		else
-		  throw Invalid_Argument();
-	    }
-	  else if ( work == "harmless" )
-	    {
-	      if ( pattr->ValueStr() == "yes" )
-		result.set_harmless( true );
-	      else
-		if ( pattr->ValueStr() == "no" )
-		  result.set_harmless (false);
-		else
-		  throw Invalid_Argument();
-	    }
-	  pattr = pattr->Next();
-	}
-      return result;
-    }
-  else
-    throw Missing_Element( SPELL_RESISTANCE );
-}
-
-std::string Spells::get_spell_description_( TiXmlElement* pspell )
-{
-  TiXmlElement* pdescription = pspell->FirstChildElement( "description" );
-  return pdescription->GetText();
-}
-
-std::string Spells::get_spell_link_( TiXmlElement* pspell )
-{
-  std::string result = pspell->Attribute( "src" );
-  return "";
-}
-
-bool Spells::check_spell_target_( TiXmlElement* pspell )
-{
-  return pspell->FirstChildElement( "target" );
-}
-
-Target* Spells::get_spell_target_( TiXmlElement* pspell )
-{
-  TiXmlElement* ptarget = pspell->FirstChildElement( "target" );
-  if ( ptarget )
-    {
-      std::string type = to_lower( ptarget->Attribute( "type" ) );
-      if ( type == "you" )
-	{
-	  Target* temp = new Target(YOU);
-	  return temp;
-	}
-      else if ( type == "special" )
-	{
-	  Target* temp = new Target(SPECIAL);
-	  temp->set_special( ptarget->Value() );
-	  return temp;
-	}
-      else
-	{
-	  Target* temp = new Target;
-	  if ( type == "creature" )
-	    temp->set_type( CREATURE );
-	  else if ( type == "living creature" )
-	    temp->set_type( LIVING_CREATURE );
-	  else if ( type == "object" )
-	    temp->set_type ( OBJECT );
-	  else
-	    throw Invalid_Argument();
-
-	  TiXmlAttribute* pattr = ptarget->FirstAttribute();
-
-	  while ( pattr )
-	    {
-	      std::string work = pattr->Name();
-	      if ( work == "distance" )
-		temp->set_distance( pattr->ValueStr() );
-	      else if ( work == "max_between" )
-		temp->set_max_between( pattr->IntValue() );
-	      else if ( work == "amount" )
-		temp->set_amount( pattr->ValueStr() );
-	      else if ( work != "type")
-		throw Invalid_Attribute();
-
-	      pattr = pattr->Next();
-	    }
-	  return temp;
-	}
-    }
-  else
-    throw Missing_Element( TARGET );
-}*/
