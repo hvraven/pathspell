c8070859637f494e8a30861390d202b7722afbef
Hendrik v. Raven <hendrik@consetetur.de>
2012-02-10 15:57:28 +0100

    added includes and namespace

--- include/elementfilterlist.h
+++ include/elementfilterlist.h
@@ -1,14 +1,18 @@
 #ifndef LIBRPG_ELEMENTFILTERLIST_H
 #define LIBRPG_ELEMENTFILTERLIST_H
 
+#include "elementlist.h"
+#include "filter.h"
+#include <vector>
+
 namespace RPG
 {
-	
+
 	template <typename T>
 	class ElementFilterList<T> : public RPG::ElementList<T>
 	{
 	public:
-		virtual void add_filter(Filter);
+		virtual void add_filter(RPG::Filter);
 		virtual void remove_filter(int);
 		virtual void clear_filter();
 		virtual void activate_filter();
@@ -18,7 +22,7 @@ namespace RPG
 		virtual bool filter_is_active();
 
 	private:
-		std::vector<Filter> filters_;
+		std::vector<RPG::Filter> filters_;
 		bool filter_state_;
 	};
 
