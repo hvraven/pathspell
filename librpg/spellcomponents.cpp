3e54d961dc74747248a7e67a9e2b81d3afc926d9
Hendrik v. Raven <hendrik@consetetur.de>
2012-02-13 19:22:59 +0100

    imported old components.cpp

---
+++ librpg/spellcomponents.cpp
@@ -0,0 +1,78 @@
+#include "spellelements.h"
+#include "error.h"
+
+Spell_Components::Spell_Components(const bool verbal, const bool somatic,
+		const bool material, const bool focus, const bool divine_focus)
+: verbal_(verbal),
+	somatic_(somatic),
+	material_(material),
+	focus_(focus),
+	divine_focus_(divine_focus)
+{
+}
+
+Spell_Components::Spell_Components(const bool verbal, const bool somatic,
+		const bool material, const bool focus, const bool divine_focus,
+		const std::string& first_description,
+		const std::string& second_description )
+: verbal_(verbal),
+	somatic_(somatic),
+	material_(material),
+	focus_(focus),
+	divine_focus_(divine_focus)
+{
+	if ( second_description != "" )
+	{
+		material_description_ = first_description;
+		focus_description_ = second_description;
+	}
+	else
+	{
+		if ( material_ == focus_ )
+			throw Unclear_Arguments();
+
+		if ( material_ )
+			material_description_ = first_description;
+		else
+			focus_description_ = first_description;
+		> }
+}
+
+std::string Spell_Components::print( const bool with_description ) const
+{
+	std::string result;
+	if ( verbal_ )
+		result += "V, ";
+	if ( somatic_ )
+		result += "S, ";
+	if ( material_ )
+	{
+		if ( with_description )
+		{
+			result += "M ("
+				+ material_description_
+				+ "), ";
+		}
+		else
+			result += "M, ";
+	}
+	if ( focus_ )
+	{
+		if ( with_description )
+		{
+			result += "F ("
+				+ focus_description_
+				+ "), ";
+		}
+		else
+			result += "F, ";
+	}
+	if ( divine_focus_ )
+		result += "DF, ";
+
+	// cut ", "
+	if ( result != "" )
+		result.erase( result.end() - 2, result.end() - 1 );
+
+	return result;
+}
