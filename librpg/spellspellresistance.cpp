96c545650867f802b5871725e6df7a80dd6f0a14
Hendrik v. Raven <hendrik@consetetur.de>
2012-02-14 16:38:07 +0100

    imported spellresistance.cpp

---
+++ librpg/spellspellresistance.cpp
@@ -0,0 +1,36 @@
+#include "spellelements.h"
+
+using namespace RPG::Pathfinder;
+
+Spell_Spell_Resistance::Spell_Spell_Resistance(const bool resistance,
+                                               const bool harmless,
+                                               const bool see_text)
+: resistance_(resistance),
+  harmless_(harmless),
+  see_text_(see_text)
+{
+}
+
+std::string Spell_Spell_Resistance::print() const
+{
+  std::string result;
+  if ( resistance_ )
+    result = "Yes";
+  else
+    result = "No";
+
+  if ( harmless_ )
+    {
+      if ( see_text_ )
+        return ( result += " (harmless, see text)" );
+      else
+        return ( result += " (harmless)" );
+    }
+  else
+    {
+      if ( see_text_ )
+        return ( result += " (see text)" );
+      else
+        return result;
+    }
+}
