4eb31fefddfa0770c4b016af3deaf1fd9b685e6b
Hendrik v. Raven <hendrik@consetetur.de>
2012-03-31 22:24:58 +0200

    restructured spell interface (only containerclass with get/set)

--- include/spell.h
+++ include/spell.h
@@ -21,24 +21,10 @@ namespace RPG
       void set_name(const std::string& name) { name_ = name; }
       void set_school(const Spell_School& school) { school_ = school; }
       void set_levels(const Spell_Level& level) { level_ = level; }
-      void add_level(const std::string& level, const int value)
-        { level_.add_level(level, value); }
       void set_casting_time(const Spell_Casting_Time& casting_time)
         { casting_time_ = casting_time; }
       void set_components(const Spell_Components& components)
         { components_ = components; }
-      void set_component_verbal(const bool value)
-        { components_.set_verbal(value); }
-      void set_component_somatic(const bool value)
-        { components_.set_somatic(value); }
-      void set_component_material(const bool value,
-                                  const std::string& description)
-        { components_.set_material(value, description); }
-      void set_component_focus(const bool value,
-                               const std::string& description)
-        { components_.set_focus(value, description); }
-      void set_component_divine_focus(const bool value)
-        { components_.set_divine_focus(value); }
       void set_range(const Spell_Range& range) { range_ = range; }
       void set_duration(const Spell_Duration& duration)
         { duration_ = duration; }
@@ -49,20 +35,17 @@ namespace RPG
       void set_description(const std::string& description)
         { description_ = description; }
 
-      const std::string& get_name() const { return name_; }
-      const Spell_School& get_school() const { return school_; }
-      const Spell_Level& get_level() const { return level_; }
-      const Spell_Casting_Time& get_casting_time() const
-        { return casting_time_; }
-      const Spell_Components& get_components() const { return components_; }
-      const Spell_Range& get_range() const { return range_; }
-      const Spell_Duration& get_duration() const { return duration_; }
-      const Spell_Saving_Throw& get_saving_throw() const
-        { return saving_throw_; }
-      const Spell_Spell_Resistance& get_spell_resistance() const
+      std::string& get_name() { return name_; }
+      Spell_School& get_school() { return school_; }
+      Spell_Level& get_level() { return level_; }
+      Spell_Casting_Time& get_casting_time() { return casting_time_; }
+      Spell_Components& get_components() { return components_; }
+      Spell_Range& get_range() { return range_; }
+      Spell_Duration& get_duration() { return duration_; }
+      Spell_Saving_Throw& get_saving_throw() { return saving_throw_; }
+      Spell_Spell_Resistance& get_spell_resistance()
         { return spell_resistance_; }
-      const std::string& get_description() const { return description_; }
-
+      std::string& get_description() { return description_; }
 
     private:
       std::string name_;
