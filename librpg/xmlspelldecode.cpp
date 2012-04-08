44a578df55cef931fcf2e7edc20851b138e10f41
Hendrik v. Raven <hendrik@consetetur.de>
2012-04-08 15:37:41 +0200

    removed inheritence from Xml_Spell_Access

--- librpg/xmlspelldecode.cpp
+++ librpg/xmlspelldecode.cpp
@@ -6,6 +6,7 @@
 #include <cctype>
 #include <map>
 
+using namespace RPG;
 using namespace RPG::Pathfinder;
 
 /**
@@ -37,80 +38,12 @@ generate_elements_map()
 }
 
 /**
- * @brief reads the elements from the xml and copies them to the Spell
- * @param pxml the element to decode
- * @return new generated spell
- *
- * @todo add a check if all necessary elements have been found
- * @todo add missing components
- * @todo add error handling
- *
- * this function checks element by element what it is and calls the respecting
- * add functions to add them to the given spell.
- */
-Spell
-Xml_Spell_Access::decode(const TiXmlElement *const pxml) const
-{
-  /* map for easy translation of the names to tokens */
-  static std::map < std::string, Spell_Token > elements;
-  if (elements.size() == 0)
-    elements = generate_elements_map();
-
-  Spell result;
-
-  TiXmlElement const * pelement = pxml->FirstChildElement();
-  while ( pelement )
-    {
-      switch( elements[ pelement->Value() ] )
-        {
-        case Spell_Token::Name:
-          decode_name(result.name, pelement);
-          break;
-        case Spell_Token::School:
-          decode_school(result.school, pelement);
-          break;
-        case Spell_Token::Level:
-          decode_level(result.levels, pelement);
-          break;
-        case Spell_Token::Casting_Time:
-          decode_casting_time(result.casting_time, pelement);
-          break;
-        case Spell_Token::Components:
-          decode_component(result.components, pelement);
-          break;
-        case Spell_Token::Range:
-          decode_range(result.range, pelement);
-          break;
-        case Spell_Token::Duration:
-          decode_duration(result.duration, pelement);
-          break;
-        case Spell_Token::Saving_Throw:
-          decode_saving_throw(result.saving_throw, pelement);
-          break;
-        case Spell_Token::Spell_Resistance:
-          decode_spell_resistance(result.spell_resistance, pelement);
-          break;
-        case Spell_Token::Description:
-          decode_description(result.description, pelement);
-          break;
-        default:
-          throw RPG::xml_error("Unknown element " +
-                               pelement->ValueStr() + " found");
-        }
-
-      pelement = pelement->NextSiblingElement();
-    }
-
-  return result;
-}
-
-/**
  * @brief reads the xml information to a name class
  * @param pelement pointer to the xml name entry
  * @see decode_name( Name& name, const TiXmlElement *const pelement )
  */
 inline std::string
-Xml_Spell_Access::decode_name(const TiXmlElement *const pelement) const
+decode_name(const TiXmlElement *const pelement)
 {
   std::string work;
   decode_name(work, pelement);
@@ -129,8 +62,8 @@ Xml_Spell_Access::decode_name(const TiXmlElement *const pelement) const
  * discarded
  */
 inline void
-Xml_Spell_Access::decode_name(std::string& name,
-                              const TiXmlElement *const pelement) const
+decode_name(std::string& name,
+                              const TiXmlElement *const pelement)
 {
   if (attribute_to_string("language", pelement) == "en")
     name = text_to_string(pelement);
@@ -142,7 +75,7 @@ Xml_Spell_Access::decode_name(std::string& name,
  * @see decode_school( School& school, const TiXmlElement *const pelement )
  */
 inline Spell_School
-Xml_Spell_Access::decode_school(const TiXmlElement *const pelement) const
+decode_school(const TiXmlElement *const pelement)
 {
   Spell_School work;
   decode_school( work, pelement );
@@ -160,8 +93,8 @@ Xml_Spell_Access::decode_school(const TiXmlElement *const pelement) const
  * correctly.
  */
 void
-Xml_Spell_Access::decode_school(Spell_School& school,
-                                const TiXmlElement *const pelement) const
+decode_school(Spell_School& school,
+                                const TiXmlElement *const pelement)
 {
   school.set_school(attribute_to_string("type", pelement));
 
@@ -184,7 +117,7 @@ Xml_Spell_Access::decode_school(Spell_School& school,
  * level
  */
 inline Spell_Levels
-Xml_Spell_Access::decode_level(const TiXmlElement *const pelement) const
+decode_level(const TiXmlElement *const pelement)
 {
   Spell_Levels work;
   decode_level( work, pelement );
@@ -201,8 +134,8 @@ Xml_Spell_Access::decode_level(const TiXmlElement *const pelement) const
  * untouched.
  */
 void
-Xml_Spell_Access::decode_level(Spell_Levels& level,
-                               const TiXmlElement *const pelement) const
+decode_level(Spell_Levels& level,
+                               const TiXmlElement *const pelement)
 {
   std::string type;
   int value = 0;
@@ -223,7 +156,7 @@ Xml_Spell_Access::decode_level(Spell_Levels& level,
  * @see decode_casting_time( Casting_Time&, const TiXmlElement *const )
  */
 inline Spell_Casting_Time
-Xml_Spell_Access::decode_casting_time(const TiXmlElement *const pelement) const
+decode_casting_time(const TiXmlElement *const pelement)
 {
   Spell_Casting_Time work;
   decode_casting_time(work, pelement);
@@ -236,8 +169,8 @@ Xml_Spell_Access::decode_casting_time(const TiXmlElement *const pelement) const
  * @param pelement pointer to the casting time in the xml
  */
 inline void
-Xml_Spell_Access::decode_casting_time(Spell_Casting_Time& time,
-                                      const TiXmlElement *const pelement) const
+decode_casting_time(Spell_Casting_Time& time,
+                                      const TiXmlElement *const pelement)
 {
   time.set(attribute_to_string("type", pelement),
            attribute_to_int("value", pelement));
@@ -249,7 +182,7 @@ Xml_Spell_Access::decode_casting_time(Spell_Casting_Time& time,
  * @see decode_component( Components&, const TiXmlElement *const )
  */
 inline Spell_Components
-Xml_Spell_Access::decode_component(const TiXmlElement *const pelement) const
+decode_component(const TiXmlElement *const pelement)
 {
   Spell_Components work;
   decode_component( work, pelement );
@@ -268,8 +201,8 @@ Xml_Spell_Access::decode_component(const TiXmlElement *const pelement) const
  * @todo add support for M/DF (with description)
  */
 void
-Xml_Spell_Access::decode_component(Spell_Components& components,
-                                   const TiXmlElement *const pelement) const
+decode_component(Spell_Components& components,
+                                   const TiXmlElement *const pelement)
 {
   std::string type = attribute_to_string("type", pelement);
   if ( type.length() == 1 )
@@ -303,7 +236,7 @@ Xml_Spell_Access::decode_component(Spell_Components& components,
  * @see decode_range( Range&, const TiXmlElement *const )
  */
 inline Spell_Range
-Xml_Spell_Access::decode_range(const TiXmlElement *const pelement) const
+decode_range(const TiXmlElement *const pelement)
 {
   Spell_Range work;
   decode_range( work, pelement );
@@ -316,8 +249,8 @@ Xml_Spell_Access::decode_range(const TiXmlElement *const pelement) const
  * @param pelement pointer to the range in the xml
  */
 void
-Xml_Spell_Access::decode_range(Spell_Range& range,
-                               const TiXmlElement *const pelement) const
+decode_range(Spell_Range& range,
+                               const TiXmlElement *const pelement)
 {
   std::string type = to_lower(attribute_to_string("type", pelement));
   if (type == "personal" || type == "touch" || type == "close" ||
@@ -333,7 +266,7 @@ Xml_Spell_Access::decode_range(Spell_Range& range,
  * @see decode_duration( Duration&, const TiXmlElement *const )
  */
 inline Spell_Duration
-Xml_Spell_Access::decode_duration(const TiXmlElement *const pelement) const
+decode_duration(const TiXmlElement *const pelement)
 {
   Spell_Duration work;
   decode_duration( work, pelement );
@@ -346,8 +279,8 @@ Xml_Spell_Access::decode_duration(const TiXmlElement *const pelement) const
  * @param pelement pointer to the duration in the xml
  */
 void
-Xml_Spell_Access::decode_duration(Spell_Duration& duration,
-                                  const TiXmlElement *const pelement) const
+decode_duration(Spell_Duration& duration,
+                                  const TiXmlElement *const pelement)
 {
   duration.set_dismissible(attribute_to_bool("dismissible", pelement));
 
@@ -368,7 +301,7 @@ Xml_Spell_Access::decode_duration(Spell_Duration& duration,
  * @see decode_saving_throw( Saving_Throw&, const TiXmlElement *const )
  */
 inline Spell_Saving_Throw
-Xml_Spell_Access::decode_saving_throw(const TiXmlElement *const pelement) const
+decode_saving_throw(const TiXmlElement *const pelement)
 {
   Spell_Saving_Throw result;
   decode_saving_throw( result, pelement );
@@ -383,8 +316,8 @@ Xml_Spell_Access::decode_saving_throw(const TiXmlElement *const pelement) const
  * @todo add support for multiple types
  */
 void
-Xml_Spell_Access::decode_saving_throw(Spell_Saving_Throw& saving_throw,
-                                      const TiXmlElement *const pelement) const
+decode_saving_throw(Spell_Saving_Throw& saving_throw,
+                                      const TiXmlElement *const pelement)
 {
   saving_throw.set_type(attribute_to_string("type", pelement));
   saving_throw.set_value(attribute_to_string("value", pelement));
@@ -399,8 +332,8 @@ Xml_Spell_Access::decode_saving_throw(Spell_Saving_Throw& saving_throw,
  * @see decode_spell_resistance( Spell_Resistance&, const TiXmlElement *const )
  */
 inline Spell_Spell_Resistance
-Xml_Spell_Access::decode_spell_resistance
-    (const TiXmlElement *const pelement) const
+decode_spell_resistance
+    (const TiXmlElement *const pelement)
 {
   Spell_Spell_Resistance result;
   decode_spell_resistance( result, pelement );
@@ -413,9 +346,9 @@ Xml_Spell_Access::decode_spell_resistance
  * @param pelement pointer to the resistance in the xml
  */
 void
-Xml_Spell_Access::decode_spell_resistance
+decode_spell_resistance
     (Spell_Spell_Resistance& spell_resistance,
-     TiXmlElement const *const pelement) const
+     TiXmlElement const *const pelement)
 {
   spell_resistance.set_resistance(attribute_to_bool("value", pelement));
   spell_resistance.set_harmless(attribute_to_bool("harmless", pelement));
@@ -428,7 +361,7 @@ Xml_Spell_Access::decode_spell_resistance
  * @see decode_description( Description&, const TiXmlElement *const )
  */
 inline std::string
-Xml_Spell_Access::decode_description(const TiXmlElement *const pelement) const
+decode_description(const TiXmlElement *const pelement)
 {
   std::string result;
   decode_description( result, pelement );
@@ -444,8 +377,8 @@ Xml_Spell_Access::decode_description(const TiXmlElement *const pelement) const
  * @todo implement language support
  */
 inline void
-Xml_Spell_Access::decode_description(std::string& description,
-                                     TiXmlElement const *const pelement) const
+decode_description(std::string& description,
+                   TiXmlElement const *const pelement)
 {
   description = text_to_string(pelement);
 }
