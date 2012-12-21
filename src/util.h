fe67fbc8ad6fd3da89abb155dc57fdeb3f7b21b6
Hendrik v. Raven <hendrik@consetetur.de>
2012-12-21 21:23:07 +0100

    added string split function

--- src/util.h
+++ src/util.h
@@ -1,6 +1,11 @@
 #ifndef PF_UTIL_H
 #define PF_UTIL_H
 
+#include <algorithm>
+#include <regex>
+#include <string>
+#include <vector>
+
 template <typename It, typename T>
 std::string
 join(It first, It last, T seperator)
@@ -15,6 +20,18 @@ join(It first, It last, T seperator)
       output += *first;
     }
   return output;
+}
+
+template <typename Container = std::vector<std::string>>
+Container
+split(const std::string& in, const std::string& seperator = "\\s+")
+{
+  std::regex rgx{seperator};
+  Container out;
+  std::copy(std::sregex_token_iterator{begin(in), end(in), rgx, -1},
+            std::sregex_token_iterator{},
+            std::back_inserter(out));
+  return out;
 }
 
 #endif /* PF_UTIL_H */
