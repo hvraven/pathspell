7f823fbdf7bdd55f9a89d2a32f498ac4ecefd180
Hendrik v. Raven <hendrik@consetetur.de>
2012-12-23 14:45:11 +0100

    use correct tolower function

--- src/util.cpp
+++ src/util.cpp
@@ -1,12 +1,13 @@
 #include "util.h"
 
 #include <algorithm>
+#include <cctype>
 
 using namespace std;
 
 string
 to_lower(string s)
 {
-  transform(begin(s), end(s), begin(s), ::tolower);
+  transform(begin(s), end(s), begin(s), (int(*)(int))tolower);
   return s;
 }
